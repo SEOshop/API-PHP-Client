f615577d9ec6a859427a091437b8b4cb49b6a8a4
Nick Edelenbos <nick.edelenbos@trimm.nl>
2015-04-09 14:24:21 +0200

    Fixed the 'Update the productAttribute resource of a shop in bulk' call

--- src/WebshopappApiClient.php
+++ src/WebshopappApiClient.php
@@ -1081,7 +1081,7 @@ class WebshopappApiResourceAttributes
     {
         $fields = array('attribute' => $fields);
 
-        return $this->client->create('attributes', $fields);
+        return $this->client->update('attributes', $fields);
     }
 
     /**
@@ -3678,11 +3678,18 @@ class WebshopappApiResourceProductsAttributes
      * @return array
      * @throws WebshopappApiException
      */
-    public function create($productId, $fields)
+    public function update($productId, $attributeId = null, $fields = array())
     {
-        $fields = array('productAttribute' => $fields);
-
-        return $this->client->create('products/' . $productId . '/attributes', $fields);
+        if (! $attributeId)
+        {
+            $fields = array('productAttributes' => $fields);
+            return $this->client->update('products/' . $productId . '/attributes', $fields);
+        }
+        else
+        {
+            $fields = array('productAttribute' => $fields);
+            return $this->client->update('products/' . $productId . '/attributes/' . $attributeId, $fields);
+        }
     }
 
     /**
